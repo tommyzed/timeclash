9c8a8cfd4924b699a5977786dae13a1d6ae63e36
Tommy Zed <dr.tommy@gmail.com>
2025-08-23 16:37:31 +0200

    Revert to b52cb186c206a28e949ac0707f7523f2ddf9798a

---
+++ scripts/alter_table.ts
@@ -0,0 +1,30 @@
+import pg from "pg";
+
+const { Pool } = pg;
+
+async function run() {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+  }
+
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  const client = await pool.connect();
+
+  try {
+    console.log("Applying database alteration...");
+    await client.query(
+      `ALTER TABLE "games" ADD COLUMN "categories" text[] NOT NULL DEFAULT '{"Politics", "Science", "History", "Culture"}'`
+    );
+    console.log("Database alteration applied successfully.");
+  } catch (err) {
+    console.error("Error applying database alteration:", err);
+  } finally {
+    client.release();
+    await pool.end();
+  }
+}
+
+run();
