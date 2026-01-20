05b238a0af484d89873ad95169406f53f896a67e
Tommy Zed <dr.tommy@gmail.com>
2026-01-20 19:15:43 +0100

    LOGS

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(async ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), ''); // Load all env vars
-  console.log(env);
+  console.log("TOMOLICK: ", env.VITE_GOOGLE_CLIENT_ID);
 
   return {
     plugins: [
